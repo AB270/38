36a85af030c3aa5ff819849485be2f254b387448
Ayush Bhardwaj <62342389+AB270@users.noreply.github.com>
2020-05-08 14:09:09 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# 38
+# CarRacingGame1.0
+Car Racing Game Stage 1.0
---
+++ sketch.js
@@ -0,0 +1,30 @@
+var canvas, backgroundImage;
+
+var gameState = 0;
+var playerCount;
+var allPlayers;
+var distance = 0;
+var database;
+
+var form, player, game;
+var cars,car1,car2,car3,car4;
+
+
+function setup(){
+  canvas = createCanvas(displayWidth,displayHeight);
+  database = firebase.database();
+  game = new Game();
+  game.getState();
+  game.start();
+}
+
+
+function draw(){
+  if(playerCount === 4){
+    game.update(1);
+  }
+  if(gameState === 1){
+    clear();
+    game.play();
+  }
+}
